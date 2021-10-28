a32ebee08c2a9e390043c45910b654033626e845
Rafael <rafaelroman35@yahoo.com>
2021-10-29 02:13:39 +0300

    tema3

---
+++ Tema3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tema3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
